064934d14a9fac3f74f2bcb64b516cdba3a5fa36
stevanmitric <mitricstevan5@gmail.com>
2025-02-17 20:08:43 +0100

    styling changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '../assets/icons/logo.svg';
 import Clock from '../assets/icons/clock.svg';
 import pdfLogo from '../assets/icons/pdf.svg';
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <header>
       <div className='header-top'>
         <div className='header-content'>
-          <nav>
-            <a href='#'>
-              <img src={Logo} alt='Logo' className='logo' />
-            </a>
+          <a href='#'>
+            <img src={Logo} alt='Logo' className='logo' />
+          </a>
+          <a href='#' className='download-mobile'>
+            <img src={pdfLogo} alt='Download' />
+            download
+          </a>
+          {/* Hamburger Button */}
+          <button className='hamburger' onClick={() => setMenuOpen(!menuOpen)}>
+            ☰
+          </button>
+
+          {/* Navigation Menu */}
+          <nav className={menuOpen ? 'nav-menu open' : 'nav-menu'}>
             <a href='#'>Die Metzgerei</a>
             <a href='#'>Dry Aged</a>
             <a href='#'>Fleischversand</a>
@@ -20,13 +32,14 @@ const Header = () => {
             <a href='#'>Tagesessen</a>
             <a href='#'>Impressum</a>
             <a href='#'>Kontakt</a>
-            <a href='#'>
+            <a href='#' className='download-desktop'>
               <img src={pdfLogo} alt='Download' />
               download
             </a>
           </nav>
         </div>
       </div>
+
       <div className='header-bottom'>
         <div className='header-content'>
           <img src={Clock} alt='Clock' />
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,14 +1,52 @@
-import React from 'react';
+import { useState } from 'react';
 import Image from '../assets/images/image 14.jpg';
+import arrowLeft from '../assets/icons/arrow-left.svg';
+import arrowRight from '../assets/icons/arrow-right.svg';
 
 const Slider = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const goToSlide = (index) => {
+    setCurrentIndex(index);
+  };
+
+  const images = [
+    '../assets/images/image 14.jpg',
+    '../assets/images/image 14.jpg',
+    '../assets/images/image 14.jpg',
+    '../assets/images/image 14.jpg',
+  ];
+
   return (
     <div>
       <div className='slider'>
         <div className='slides'>
+          <div className='slides-content'>
+            <div className='slides-content-text'>
+              <h2>Heiko Brath Metzgermeister</h2>
+              <p>
+                Deutsches Ipsum Dolor deserunt dissentias Grimms Märchen et.
+                Tollit argumentum ius an. Pfannkuchen lobortis elaboraret per
+                ne, nam Aperol Spritz probatus pertinax.
+              </p>
+            </div>
+          </div>
           <img src={Image} alt='Image 1' />
           <img src={Image} alt='Image 2' />
           <img src={Image} alt='Image 3' />
+        </div>
+      </div>
+      <div>
+        <div className='pagination'>
+          <img src={arrowLeft} alt='' />
+          {images.map((_, index) => (
+            <span
+              key={index}
+              className={`dot ${index === currentIndex ? 'active' : ''}`}
+              onClick={() => goToSlide(index)}
+            ></span>
+          ))}
+          <img src={arrowRight} alt='' />
         </div>
       </div>
     </div>
